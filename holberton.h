5382a3fc799435846943ee093273c9bc4b97ffa1
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-07-27 16:39:39 +0300

    Header

--- holberton.h
+++ holberton.h
@@ -1,5 +1,26 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
-int _putchar(char c);
+#ifndef __HOLBERTONH__
+#define __HOLBERTONH__
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <limits.h>
+/**
+ * struct print - printf functionparameters
+ * @a: conversion specifier to match
+ * @f: function to return
+ *
+ */
+typedef struct print
+{
+	char *a;
+	int (*f)(va_list);
+} _types;
+
+int putPercent(char s, char n);
+int printInt(va_list i);
+int printstring(va_list s);
+int printchar(va_list c);
 int _printf(const char *format, ...);
-#endif /*HOLBERTON.H*/
+int _putchar(char g);
+#endif
