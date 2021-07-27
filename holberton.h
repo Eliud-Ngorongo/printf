940e7e13a2d4b9d54fefad76e7f32b3e3464e856
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-07-27 18:04:09 +0300

    New

--- holberton.h
+++ holberton.h
@@ -1,32 +1,34 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
-
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#include <unistd.h>
+#include <stdlib.h>
 #include <stdarg.h>
+#include <limits.h>
+
 /**
- * struct print - structure for printing various types
- * @t: type to print
- * @f: function to print
+ * struct format_func- functions of formating text
+ *
+ * @c: argument character
+ * @f: formating function
  */
-typedef struct print
+
+typedef struct format_func
 {
-	char *t;
-	int (*f)(va_list);
-} print_t;
+	char *c;
+	int (*f)();
+} print_fx;
 
-int _putchar(char c);
 int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_u(va_list u);
-int print_b(va_list b);
-int print_o(va_list o);
-int print_x(va_list x);
-int print_X(va_list X);
-int print_p(va_list p);
-int print_S(va_list S);
-int print_r(va_list r);
-int print_R(va_list R);
+int aux_func(const char *format, va_list args, print_fx *fx);
+int print_c(va_list args);
+int print_s(va_list args);
+int print_i(va_list args);
+int print_b(va_list args);
+int print_o(va_list args);
+int print_X(va_list args);
+int print_x(va_list args);
+int print_r(va_list args);
+int print_R(va_list args);
+int _putchar(char c);
 
-#endif  /* _HOLBERTON_H */
+#endif
