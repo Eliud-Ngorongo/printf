a153d9f21b89882c47b4f9b357e66927a46e67ba
Eliud-Ngorongo <eliudnjenga@gmail.com>
2021-07-28 14:23:50 +0300

    Take two

--- holberton.h
+++ holberton.h
@@ -24,11 +24,6 @@ int print_c(va_list args);
 int print_s(va_list args);
 int print_i(va_list args);
 int print_b(va_list args);
-int print_o(va_list args);
-int print_X(va_list args);
-int print_x(va_list args);
-int print_r(va_list args);
-int print_R(va_list args);
 int _putchar(char c);
 
 #endif
